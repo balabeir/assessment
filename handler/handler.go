b27f3f34a4f933b5634bfe08a9ff12ae94a46f98
wtpJ <wutthiphongj@donuts-bkk.com>
2022-12-25 22:19:45 +0700

    initial project

---
+++ handler/handler.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"github.com/labstack/echo"
+)
+
+func New() *echo.Echo {
+	e := echo.New()
+
+	// write handler here
+
+	return e
+}
--- server.go
+++ server.go
@@ -1,11 +1,39 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/balabeir/assessment/handler"
+	"github.com/labstack/echo"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	h := handler.New()
+
+	// start server
+	go func() {
+		if err := h.Start(":" + os.Getenv("PORT")); err != nil && err != http.ErrServerClosed {
+			h.Logger.Fatal("shutting down the server")
+		}
+	}()
+
+	shutdown(h)
+}
+
+func shutdown(h *echo.Echo) {
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
+	<-shutdown
+	fmt.Println("server shutting down...")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := h.Shutdown(ctx); err != nil {
+		h.Logger.Fatal(err)
+	}
 }
